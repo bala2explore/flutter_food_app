6d201872c5c74f2477bb96101cd0b08ba2215c64
bala2explore <bala2explore@gmail.com>
2020-08-03 17:20:06 +0530

    Initial commit

---
+++ lib/result.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class Result extends StatelessWidget {
+  final int resultScore;
+  final Function resetHandler;
+
+  Result(this.resultScore, this.resetHandler);
+
+  String get resultPhrase {
+    String resultText;
+    if (resultScore == 30) {
+      resultText = 'You are Mass';
+    } else if (resultScore >5 && resultScore <=29 ) {
+      resultText = 'Paravaalai';
+    } else if (resultScore <=5) {
+      resultText = 'Sumar moonji kumar';
+    } else {
+      resultText = 'You are ok';
+    }
+    return resultText;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: <Widget>[
+          Text(
+            resultPhrase,
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          FlatButton(
+            child: Text(
+              'Restart Quiz!',
+            ),
+            textColor: Colors.blue,
+            onPressed: resetHandler,
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,9 +10,29 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.orange,
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text("Food App"),
+        leading: IconButton(
+            icon: Icon(
+              Icons.supervisor_account,
+              color: Colors.white,
+            ),
+            iconSize: 30,
+            onPressed: null),
+        actions: <Widget>[
+          FlatButton(
+              onPressed: null,
+              child: Text(
+                "Cart",
+                style: TextStyle(color  : Colors.white),
+              ))
+        ],
+      ),
+      body: ListView(
+        children: <Widget>[
+          TextField()
+        ],
       ),
     );
   }
